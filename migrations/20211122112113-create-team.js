f3ab2b4ddf0891512a0b87cea42107ad0823792f
jyang1003git config --global user.name <jerryyang1003@gmail.com>
2022-01-10 12:22:28 -0500

    changed migrations

--- migrations/20211122112113-create-team.js
+++ migrations/20211122112113-create-team.js
@@ -18,11 +18,11 @@ module.exports = {
         type: Sequelize.BOOLEAN
       },
       createdAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       }
     });
